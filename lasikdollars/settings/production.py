8bdc914f3f5e988d7ad7198013a4d4222166faee
aier-usa <aier.usa@outlook.com>
2017-04-06 22:41:19 -0500

    SECRET_KEY

--- lasikdollars/settings/production.py
+++ lasikdollars/settings/production.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = \
-#    '8b*i*v9ty@%!=isjtw6fht#u%!9g9-z@9+h#bfqyvi4y!z51kw'
+SECRET_KEY = \
+    '8b*i*v9ty@%!=isjtw6fht#u%!9g9-z@9+h#bfqyvi4y!z51kw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
